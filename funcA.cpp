30e2faa6edc36341c351be59d58e51c9b9433fa2
Nazar <senyshyn.nazar@gmail.com>
2025-01-05 20:06:48 +0200

    Added comments, changed the func

--- funcA.cpp
+++ funcA.cpp
@@ -1,13 +1,18 @@
 #include "funcA.h"
 #include <cmath>
 
+/*
+x - double, input argument param
+n - number of loops on elements
+*/
 double FuncA::compute(double x, int n) {
-    double result = 0;
+    double result = 0.0;
     
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 1) { 
-            result += (i == 1) ? x : (pow(x, i) / ((i % 4 == 3) ? (i - 1) : i));
-        }
+    for (int i = 0; i < n; ++i) {
+        double term = (std::tgamma(2 * i + 1 + 1) /
+                       (std::pow(4, i) * std::pow(std::tgamma(i + 1), 2) * (2 * i + 1)))
+                       * std::pow(x, 2 * i + 1);
+        result += term;
     }
     
     return result;
